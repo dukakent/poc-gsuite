015be0e87aa67e1bc223f97d64f01e96d2b6ec7e
Inokentii Duka <inokentii.duka@valor-software.com>
2018-06-21 17:01:54 +0300

    get events of specific calendar

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,66 +1,110 @@
-import { Get, Controller, Req, Res, Query } from '@nestjs/common';
-import { google } from 'googleapis';
+import { Get, Controller, Request, Response, Query, Param } from '@nestjs/common';
+import { admin_directory_v1, calendar_v3, google } from 'googleapis';
 import { OAuthOptions } from '../tmp/credentials';
 import { AxiosError } from '@nestjs/common/http/interfaces/axios.interfaces';
+import Schema$Users = admin_directory_v1.Schema$Users;
+import Schema$Events = calendar_v3.Schema$Events;
 
 @Controller()
 export class AppController {
-  private oauth2Client: any;
+  private readonly googleOauth2Client = new google.auth.OAuth2(
+    OAuthOptions.clientId,
+    OAuthOptions.clientSecret,
+    OAuthOptions.redirectUri,
+  );
 
-  private readonly OAuthOptions = OAuthOptions;
+  private readonly googleAdminClient = google.admin('directory_v1');
+  private readonly googleCalendarClient = google.calendar({version: 'v3', auth: this.googleOauth2Client});
 
-  constructor() {}
+  private readonly scopes = [
+    'https://www.googleapis.com/auth/admin.directory.user.readonly',
+    'https://www.googleapis.com/auth/calendar.readonly',
+  ];
+
+  private readonly oauthURL = this.googleOauth2Client.generateAuthUrl({ scope: this.scopes });
 
   @Get()
-  root(@Req() req, @Res() res): void {
-    this.oauth2Client = new google.auth.OAuth2(
-      this.OAuthOptions.clientId,
-      this.OAuthOptions.clientSecret,
-      this.OAuthOptions.redirectUri,
-    );
-
-    const scope = [
-      'https://www.googleapis.com/auth/admin.directory.user',
-      'https://www.googleapis.com/auth/admin.directory.user.readonly',
-    ];
-
-    const url = this.oauth2Client.generateAuthUrl({ scope });
-
-    res.redirect(url);
+  root(@Request() req, @Response() res): void {
+    res.redirect(this.oauthURL);
   }
 
   @Get('/authcallback')
-  async auth(@Query() queryParams) {
+  async auth(@Request() req, @Response() res, @Query() queryParams) {
     const { code } = queryParams;
 
-    const { tokens } = await this.oauth2Client.getToken(code);
+    const { tokens } = await this.googleOauth2Client.getToken(code);
 
-    this.oauth2Client.setCredentials(tokens);
+    this.googleOauth2Client.setCredentials(tokens);
 
-    this.listUsers()
+    this.getUserList()
       .catch((e: AxiosError) => {
-        console.log('ERROR: ', e);
+        res.send(`ERROR: ${e.code}`);
+
+        throw e;
       })
-      .then(users => {
-        console.log('Users: ', users);
+      .then(users => res.send(this.compileUserListTemplate(users.data)));
+  }
+
+  @Get('/calendar/:email')
+  async getCalendar(@Request() req, @Response() res, @Param('email') email) {
+    this.getEvents(email)
+      .catch(e => {
+        res.send(`ERROR: ${e.code}`);
+
+        throw e;
+      })
+      .then(events => {
+        res.send(this.compileEventsTemplate(email, events.data));
       });
   }
 
-  private listUsers(): Promise<any> {
-    const service = google.admin('directory_v1');
-
-    return new Promise((resolve, reject) => {
-      service.users.list({
-        auth: this.oauth2Client,
-        orderBy: 'email',
-        domain: 'valor-software.com',
-      }, (err, response) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(response);
-        }
-      });
+  private getUserList(): Promise<any> {
+    return this.googleAdminClient.users.list({
+      auth: this.googleOauth2Client,
+      customer: 'my_customer',
     });
+  }
+
+  private getEvents(email: string): Promise<any> {
+    return this.googleCalendarClient.events.list({
+      calendarId: email,
+      maxResults: 99,
+    });
+  }
+
+  private compileUserListTemplate(usersResponse: Schema$Users): string {
+    const users = usersResponse.users.map(user => `
+      <p>
+        <span>
+          <div>${user.name.fullName}</div>
+          <div>${user.primaryEmail}</div>
+        </span>
+        <span>
+          <a href="/calendar/${user.primaryEmail}">Get his calendar events</a>
+        </span>
+      </p>
+    `).join('');
+
+    return `
+      <p><h3>Users</h3></p>
+      ${users}
+    `;
+  }
+
+  private compileEventsTemplate(email: string, events: Schema$Events): string {
+    const items = events.items.map(item => `
+      <p style="border-bottom: 1px solid #333">
+        <div>${item.summary}</div>
+        <div>${item.description || ''}</div>
+        <div>${item.start.dateTime}</div>
+      </p>
+    `).join('');
+
+    return `
+      <p>
+        <h3>Events of ${email}</h3>
+      </p>
+      ${items}
+    `;
   }
 }
